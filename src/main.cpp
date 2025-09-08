203afb3299a3398460556bb84548a7a1f8111d41
Czebosak <czebosak@proton.me>
2025-09-08 22:54:36 +0200

    Use Mesh3D::cube

--- src/main.cpp
+++ src/main.cpp
@@ -83,68 +83,15 @@ int main(void) {
         glm::perspective(glm::radians(45.0f), 1920.0f/1080.0f, 0.1f, 1000.0f)
     );
 
-    std::vector<Vertex3D> vertices = {
-        // Front face (normal +Z)
-        {{-0.5f, -0.5f,  0.5f}, { 0.0f,  0.0f,  1.0f}, {0.0f, 0.0f}}, // 0
-        {{ 0.5f, -0.5f,  0.5f}, { 0.0f,  0.0f,  1.0f}, {1.0f, 0.0f}}, // 1
-        {{ 0.5f,  0.5f,  0.5f}, { 0.0f,  0.0f,  1.0f}, {1.0f, 1.0f}}, // 2
-        {{-0.5f,  0.5f,  0.5f}, { 0.0f,  0.0f,  1.0f}, {0.0f, 1.0f}}, // 3
-
-        // Back face (normal -Z)
-        {{ 0.5f, -0.5f, -0.5f}, { 0.0f,  0.0f, -1.0f}, {0.0f, 0.0f}}, // 4
-        {{-0.5f, -0.5f, -0.5f}, { 0.0f,  0.0f, -1.0f}, {1.0f, 0.0f}}, // 5
-        {{-0.5f,  0.5f, -0.5f}, { 0.0f,  0.0f, -1.0f}, {1.0f, 1.0f}}, // 6
-        {{ 0.5f,  0.5f, -0.5f}, { 0.0f,  0.0f, -1.0f}, {0.0f, 1.0f}}, // 7
-
-        // Left face (normal -X)
-        {{-0.5f, -0.5f, -0.5f}, {-1.0f,  0.0f,  0.0f}, {0.0f, 0.0f}}, // 8
-        {{-0.5f, -0.5f,  0.5f}, {-1.0f,  0.0f,  0.0f}, {1.0f, 0.0f}}, // 9
-        {{-0.5f,  0.5f,  0.5f}, {-1.0f,  0.0f,  0.0f}, {1.0f, 1.0f}}, // 10
-        {{-0.5f,  0.5f, -0.5f}, {-1.0f,  0.0f,  0.0f}, {0.0f, 1.0f}}, // 11
-
-        // Right face (normal +X)
-        {{ 0.5f, -0.5f,  0.5f}, { 1.0f,  0.0f,  0.0f}, {0.0f, 0.0f}}, // 12
-        {{ 0.5f, -0.5f, -0.5f}, { 1.0f,  0.0f,  0.0f}, {1.0f, 0.0f}}, // 13
-        {{ 0.5f,  0.5f, -0.5f}, { 1.0f,  0.0f,  0.0f}, {1.0f, 1.0f}}, // 14
-        {{ 0.5f,  0.5f,  0.5f}, { 1.0f,  0.0f,  0.0f}, {0.0f, 1.0f}}, // 15
-
-        // Top face (normal +Y)
-        {{-0.5f,  0.5f,  0.5f}, { 0.0f,  1.0f,  0.0f}, {0.0f, 0.0f}}, // 16
-        {{ 0.5f,  0.5f,  0.5f}, { 0.0f,  1.0f,  0.0f}, {1.0f, 0.0f}}, // 17
-        {{ 0.5f,  0.5f, -0.5f}, { 0.0f,  1.0f,  0.0f}, {1.0f, 1.0f}}, // 18
-        {{-0.5f,  0.5f, -0.5f}, { 0.0f,  1.0f,  0.0f}, {0.0f, 1.0f}}, // 19
-
-        // Bottom face (normal -Y)
-        {{-0.5f, -0.5f, -0.5f}, { 0.0f, -1.0f,  0.0f}, {0.0f, 0.0f}}, // 20
-        {{ 0.5f, -0.5f, -0.5f}, { 0.0f, -1.0f,  0.0f}, {1.0f, 0.0f}}, // 21
-        {{ 0.5f, -0.5f,  0.5f}, { 0.0f, -1.0f,  0.0f}, {1.0f, 1.0f}}, // 22
-        {{-0.5f, -0.5f,  0.5f}, { 0.0f, -1.0f,  0.0f}, {0.0f, 1.0f}}, // 23
-    };
-
-    std::vector<u32> indices = {
-        // Front
-        0, 1, 2,  2, 3, 0,
-        // Back
-        4, 5, 6,  6, 7, 4,
-        // Left
-        8, 9, 10, 10, 11, 8,
-        // Right
-        12, 13, 14, 14, 15, 12,
-        // Top
-        16, 17, 18, 18, 19, 16,
-        // Bottom
-        20, 21, 22, 22, 23, 20
-    };
-
     glm::mat4 cube_transform = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -20.0f));
     Texture cube_texture("/home/czebosak/Development/cpp/graphics/opengl/terrain/assets/textures/cube.png");
     Shader cube_shader("/home/czebosak/Development/cpp/graphics/opengl/terrain/assets/shaded.glsl");
     cube_shader.bind();
     cube_shader.set_uniform_1i("u_material.diffuse", 0);
-    cube_shader.set_uniform_v3("u_material.specular", 1.0f, 1.0f, 1.0f);
+    //cube_shader.set_uniform_v3("u_material.specular", 1.0f, 1.0f, 1.0f);
     cube_shader.set_uniform_1f("u_material.shininess", 32.0f);
     cube_shader.set_uniform_mat4f("u_model", cube_transform);
-    Mesh3D cube(std::move(vertices), std::move(indices));
+    Mesh3D cube = Mesh3D::cube();
 
     cube_shader.set_uniform_v3("u_sun.direction", -1.0f, -1.0f, 0.5f);
     cube_shader.set_uniform_v3("u_sun.diffuse", 1.0f, 1.0f, 0.9f);
